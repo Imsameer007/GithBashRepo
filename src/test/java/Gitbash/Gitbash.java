f2c992e499192253749cde558e9a7f70ffcbfa9a
Imsameer007 <sahilsameer77@gmail.com>
2023-09-28 15:27:51 +0530

    3 commit

--- src/test/java/Gitbash/Gitbash.java
+++ src/test/java/Gitbash/Gitbash.java
@@ -1,5 +1,13 @@
 package Gitbash;
 
+import org.testng.annotations.Test;
+
 public class Gitbash {
+	@Test
+	public void gitbashMethod() {
+		System.out.println("hi");
+		System.out.println("Good Morning");
+		System.out.println("");
+	}
 
 }
